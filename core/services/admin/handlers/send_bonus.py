f0154c90cbc9780d27e9c01c95827f11f97d7d9e
Minimalon <minimalonchik@gmail.com>
2025-02-11 11:19:18 +0300

    .

--- core/services/admin/handlers/send_bonus.py
+++ core/services/admin/handlers/send_bonus.py
@@ -4,13 +4,9 @@ from aiogram.types import Message, CallbackQuery
 
 from core.artix.CS.cs import CS
 from core.artix.CS.pd_model import Asset, AssetType, AwardsType
-from core.artix.cash.querys import Artix
-from core.artix.foreman.foreman import get_cashes, get_cash
 from core.loggers.bot_logger import Logger
 from core.utils import texts
-from ..callback_data import ShopCreateUser
-from ..keyboards import inline
-from ..states import CreateUserState, SendBonusState
+from ..states import SendBonusState
 
 router = Router()
 
@@ -21,16 +17,19 @@ async def send_bonuses(call: CallbackQuery, state: FSMContext, log: Logger):
     await call.message.edit_text('Введите номер карты пользователя')
     await state.set_state(SendBonusState.idcard)
 
+
 @router.message(SendBonusState.idcard)
 async def send_bonuses_idcard(message: Message, state: FSMContext, log: Logger):
     log.info(f'Номер карты {message.text}')
     if not message.text.isdigit():
-        await message.answer(texts.error_head + 'Номер карты должен содержать только цифры\nВведите номер карты еще раз')
+        await message.answer(
+            texts.error_head + 'Номер карты должен содержать только цифры\nВведите номер карты еще раз')
         log.error('Номер карты должен содержать только цифры')
         return
     await state.update_data(idcard_award=message.text)
     await message.answer('Введите сумму бонусов в рублях без копеек')
     await state.set_state(SendBonusState.award)
+
 
 @router.message(SendBonusState.award)
 async def send_bonuses_award(message: Message, state: FSMContext, log: Logger):
@@ -52,9 +51,11 @@ async def send_bonuses_award(message: Message, state: FSMContext, log: Logger):
         }
     ))
     if response.ok:
-        await message.answer(texts.success_head + 'Бонусы успешно зачислены')
-        log.success('Бонусы успешно зачислены')
+        await message.answer(
+            texts.success_head + f'Успешно зачислено {message.text} бонусов клиенту на карту {data.get("idcard_award")}')
+        log.success(f'Успешно зачислено {message.text} бонусов клиенту на карту {data.get("idcard_award")}')
     else:
         await message.answer(f"{texts.error_head}Бонусы не были зачислены\n{await response.text()}")
+        log.error(f'Не зачислено {message.text} бонусов клиенту на карту {data.get("idcard_award")}')
         log.error(await response.text())
     await state.clear()
